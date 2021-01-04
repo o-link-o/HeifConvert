b39be7595d781d26de607484daa770d105baf979
Oliver Link <oliver.link@scout24.ch>
2021-01-04 18:25:41 +0100

    Add project files.

---
+++ HeifConvert/Converter.cs
@@ -0,0 +1,42 @@
+﻿using ImageMagick;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeifConvert
+{
+    public class Converter
+    {
+        public void Start(string path, bool includeSubfolder, bool overwrite, bool deleteSource)
+        {
+            if (includeSubfolder)
+            {
+                var subDirectories = Directory.GetDirectories(path);
+                if (subDirectories?.Length > 0)
+                {
+                    foreach (var subDirectory in subDirectories)
+                        Start(subDirectory, includeSubfolder, overwrite, deleteSource);
+                }
+            }
+            Parallel.ForEach(Directory.GetFiles(path), new ParallelOptions { MaxDegreeOfParallelism = 3 }, (file) =>
+            {
+                var extension = Path.GetExtension(file);
+                if (extension.ToLower() == ".heic")
+                {
+                    var destination = Path.Combine(path, $"{Path.GetFileNameWithoutExtension(file)}.jpg");
+                    if (overwrite || !File.Exists(Path.Combine(path, $"{Path.GetFileNameWithoutExtension(file)}.jpg")))
+                    {
+                        using (var image = new MagickImage(file))
+                        {
+                            image.Write(destination);
+                        }
+                    }
+                    if (deleteSource)
+                        File.Delete(file);
+                }
+            });
+        }
+    }
+}
---
+++ HeifConvert/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace HeifConvert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var converter = new Converter();
+            if (args.Length == 0)
+            {
+                Console.WriteLine("add path and optional parameters (/o = overwrite, /s = include subfolders, /d = delete source)");
+                return;
+            }
+            else
+            {
+                if (!Directory.Exists(args[0]))
+                {
+                    Console.WriteLine("invalid path. first argument must be a valid path");
+                    return;
+                }
+            }
+            bool includeSubFolders = args.Contains("/s");
+            bool overwrite = args.Contains("/o");
+            bool deleteSource = args.Contains("/d");
+            converter.Start(args[0], includeSubFolders, overwrite, deleteSource);
+        }
+    }
+}
